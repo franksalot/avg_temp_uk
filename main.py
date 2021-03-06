26082281e95f8802f63ef676b8555c46f41b1765
Frank FrankArregger-Powell <frank@bulb.co.uk>
2021-03-06 11:15:37 +0000

    add serve

--- main.py
+++ main.py
@@ -40,4 +40,4 @@ if __name__ == '__main__':
     #port = int(os.environ.get('PORT', 5000))
     #app.run_server(debug=False)
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run_server(host='0.0.0.0', port=port)
